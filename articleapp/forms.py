2a0635df0cca7527ed579933ca10afb604a322f6
Lks9172 <ekdan9172@naver.com>
2021-02-22 13:25:23 +0900

    위지위그 적용!

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,10 +1,14 @@
 from django.db.models import TextField
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
 
 
 class CreateArticleForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height: auto;'}))
+
     class Meta:
         model = Article
         fields = ['title', 'introduce', 'content','deadline','question', 'thumbnail']# '__all__' 설정시 전체 필드 추가
